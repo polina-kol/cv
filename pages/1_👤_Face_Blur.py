f288e3bf2f9963404437b0bc5bc78c701104abde
polina-kol <kolokolovapolina3@gmail.com>
2025-06-06 12:26:52 +0300

    s

--- pages/1_👤_Face_Blur.py
+++ pages/1_👤_Face_Blur.py
@@ -58,8 +58,7 @@ with tab1:
 
 with tab2:
     st.header("YOLO face detector + OpenCV Blur")
-    st.image("assets/pr_curve_face.jpg", caption="PR-кривая")
-    st.image("assets/conf_face.png", caption="Confusion matrix")
+    st.image("assets/conf_face.jpg", caption="Confusion matrix")
     st.image("assets/pr_face.png", caption="PR-curve")
-    st.image("assets/loss_metrics_face.png", caption="Losses and metrics")
+    st.image("assets/loss_metrics_face.jpg", caption="Losses and metrics")
 
